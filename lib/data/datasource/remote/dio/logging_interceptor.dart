af6f8f1ec945f204614b2f89ba8c27bc4a847798
MAlazhariy <mostafa.maok2000@gmail.com>
2023-08-26 21:24:48 +0300

    [fix] logging_interceptor.dart on request post and sending form data.

--- lib/data/datasource/remote/dio/logging_interceptor.dart
+++ lib/data/datasource/remote/dio/logging_interceptor.dart
@@ -7,14 +7,18 @@ import 'package:flutter/material.dart';
 class LoggingInterceptor extends InterceptorsWrapper {
   final int maxCharactersPerLine = 200;
 
-
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    debugPrint("--> ${options.method} ${options.baseUrl}${options.path}");
+    debugPrint("--> ${options.method} ${options.path}");
     debugPrint("Headers: ${options.headers}");
     debugPrint("parameters: ${options.queryParameters}");
-    if(options.data != null){
-      debugPrint("body data: ${json.encode(options.data)}");
+    if (options.data != null) {
+      debugPrint("body data: ${options.data}");
+      // try{
+      //   debugPrint("body data: ${json.encode(options.data)}");
+      // } catch (e) {
+      //   debugPrint("body data: ${options.data}");
+      // }
     }
     return super.onRequest(options, handler);
   }
@@ -28,9 +32,11 @@ class LoggingInterceptor extends InterceptorsWrapper {
   }
 
   @override
-  void onError(DioError err, ErrorInterceptorHandler handler) {
-    log("!!! API ERROR [${err.response?.statusCode}] => path: ${err.requestOptions.path}");
-    debugPrint("error message: ${err.message}\nresponse: ${err.response}");
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    log("!!! API ERROR [${err.response?.statusCode}] => ${err.requestOptions.path}\n"
+        "! error message: ${err.message}\n"
+        "! response: ${err.response}\n"
+        "!!! --------");
     return super.onError(err, handler);
   }
 }
