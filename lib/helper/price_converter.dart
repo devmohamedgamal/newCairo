e4a11effb80ce93fdf1133555267cb057ccdea58
MAlazhariy <mostafa.maok2000@gmail.com>
2023-09-11 03:02:07 +0300

    [reformat] price_converter.dart

--- lib/helper/price_converter.dart
+++ lib/helper/price_converter.dart
@@ -2,16 +2,16 @@
 import 'package:flutter/material.dart';
 
 class PriceConverter {
-  static String calculateTotal(BuildContext context, String price,int quantity) {
-  double total = 0.0;
-  total = double.parse(price) * quantity;
-  return total.toString();
+  static String calculateTotal(BuildContext context, String price, int quantity) {
+    double total = 0.0;
+    total = double.parse(price) * quantity;
+    return total.toString();
   }
 
   static double convertWithDiscount(BuildContext context, double price, double discount, String discountType) {
-    if(discountType == 'amount' || discountType == 'flat') {
+    if (discountType == 'amount' || discountType == 'flat') {
       price = price - discount;
-    }else if(discountType == 'percent' || discountType == 'percentage') {
+    } else if (discountType == 'percent' || discountType == 'percentage') {
       price = price - ((discount / 100) * price);
     }
     return price;
@@ -19,16 +19,16 @@ class PriceConverter {
 
   static double calculation(double amount, double discount, String type, int quantity) {
     double calculatedAmount = 0;
-    if(type == 'amount' || type == 'flat') {
+    if (type == 'amount' || type == 'flat') {
       calculatedAmount = discount * quantity;
-    }else if(type == 'percent' || type == 'percentage') {
+    } else if (type == 'percent' || type == 'percentage') {
       calculatedAmount = (discount / 100) * (amount * quantity);
     }
     return calculatedAmount;
   }
 
-  // static String percentageCalculation(BuildContext context, double price, double discount, String discountType) {
-  //   return '${(discountType == 'percent' || discountType == 'percentage') ? '$discount %'
-  //       : convertPrice(context, discount)} OFF';
-  // }
+// static String percentageCalculation(BuildContext context, double price, double discount, String discountType) {
+//   return '${(discountType == 'percent' || discountType == 'percentage') ? '$discount %'
+//       : convertPrice(context, discount)} OFF';
+// }
 }
